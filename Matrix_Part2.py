c5b263dd788d166d487e34ad0c04dac562c19b7f
Amit Daiman <42063883+daimanamit@users.noreply.github.com>
2021-11-28 00:00:59 +0100

    Add files via upload

---
+++ Matrix_Part2.py
@@ -0,0 +1,20 @@
+##Amit Daiman, Locker Number - 375
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 22 19:05:09 2019
+
+@author: ada003
+"""
+A = [[11, 12, 13],
+    [14, 15, 16],
+    [17, 18, 19]]
+Symmetric = True
+for x in range(len(A)):
+    for y in range(len(A[x])):
+        if A[x][y] != A[y][x]:
+            Symmetric = False
+            break
+        print("A is NON-SYMMETRIC")
+    if not Symmetric:
+        break
+        print("A is SYMMETRIC")
